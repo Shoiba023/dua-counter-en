eddebf18b37d7c0a330c84e5d197c6683501509a
Shoiba Ali <shoibaa320@gmail.com>
2025-05-21 18:15:57 -0700

    Fixed: clean credential block and sheet setup

--- utils/google_sheet.py
+++ utils/google_sheet.py
@@ -1,17 +1,22 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+import json
 from dotenv import load_dotenv
 
 # .env فائل سے API کی معلومات حاصل کریں
 load_dotenv()
 
 SHEET_NAME = os.getenv("SHEET_NAME")
-CREDENTIALS_FILE = os.getenv("GOOGLE_CREDENTIALS_JSON")
 
 # Google Sheet کنیکشن
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
+
+# JSON credentials string کو env سے حاصل کریں اور dictionary میں convert کریں
+creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+creds_dict = json.loads(creds_json)
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open(SHEET_NAME).sheet1
 
